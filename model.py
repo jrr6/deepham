262976f92f97aa5514d4d1d131bcd75ba37aefee
Hammad Izhar <hammad_izhar@brown.edu>
2023-04-23 17:09:22 -0400

    begin scaffolding model.py
    Co-authored-by: jrr6 <jrr6@users.noreply.github.com>
    Co-authored-by: Ed Bielawa <ebielawa@users.noreply.github.com>

--- model.py
+++ model.py
@@ -1,19 +1,59 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-from torch import nn
+from torch_geometric.data import Data
+from torch_geometric.nn import GCNConv
+from MLP import MLP
 
 
 class DeepHam(nn.Module):
-    def __init__(self):
-        super().__init__()
-        pass
+    def __init__(self, embed_size=16):
+        super(DeepHam, self).__init__()
 
-    def forward(self, x):
-        pass
+        ### Hyperparameters ###
+        self.node_embedding_size = embed_size
+        #######################
+
+        self.conv1 = GCNConv()
+        self.conv2 = GCNConv()
+        self.conv3 = GCNConv()
+
+        self.predictor = MLP()
+
+    def forward(self, vertices, edge_index):
+        out = []
+        original_vertices = vertices.detach()  # breaks autograd connection
+
+        # FIXME: do something more thoughtful (this breaks our loss)
+        for _ in range(len(original_vertices)):
+
+            # TODO: Call GNNs here
+            vertices = self.conv1(vertices, edge_index)
+            vertices = vertices.tanh()
+            vertices = self.conv2(vertices, edge_index)
+            vertices = vertices.tanh()
+            vertices = self.conv3(vertices, edge_index)
+            vertices = vertices.tanh()
+
+            probs = self.predictor(vertices)
+
+            chosen_vertex = vertices[torch.argmax(probs)]
+
+            out.append(chosen_vertex)
+
+            # TODO: delete chosen vertex (and its incident edges)
+
+        return out
 
 # TODO: Come up with a better name
 
 
-class Loss(nn.Module):
+class Dist_Loss(nn.Module):
     def __init__(self):
-        super(Loss, self).__init__()
+        super(Dist_Loss, self).__init__()
+
+    # Computes difference in length between two tensors
+    # Squared (because that's a good idea)
+    def forward(self, X: nn.Tensor, Y: nn.Tensor):
+        return (len(X) - len(Y)) ** 2
