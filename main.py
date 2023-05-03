94158070034283f53b2a0475fde035597634d6a0
jrr6 <7482866+jrr6@users.noreply.github.com>
2023-05-03 19:49:43 -0400

    Various visualization improvements

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import networkx as nx
 import torch_geometric as pyg
 import torch
 import numpy as np
+from IPython.display import display, clear_output
 torch.manual_seed(0)
 np.random.seed(0)
 
@@ -47,8 +48,18 @@ def run_episode(model: DeepHamModel, env: GraphState, optimizer: torch.optim.Opt
     optimizer.step()
     return loss
 
+def run_random_episode(model, env: GraphState, optimizer, criterion) -> torch.Tensor:
+    env = env.reset(new_graph=False)
+    done = False
+    while not done:
+        options, _, _, _ = pyg.utils.k_hop_subgraph(int(env.curr_vertex_index), 1, env.graph.edge_index)  # type: ignore
+        options = options.detach().numpy()
+        options = np.delete(options, np.where(options == int(env.curr_vertex_index)))
+        opt = np.random.choice(options).item()
+        _, _, done, _ = env.step(opt)
+    return torch.tensor(0)
 
-def main():
+def train_model(visualize=True, notebook=False, random=False):
     # corpus = load_corpus(DATA_PATH)
 
     model = DeepHamModel()
@@ -60,33 +71,57 @@ def main():
     model.train()
 
     fig, [loss_ax, length_ax, graph_ax] = plt.subplots(1, 3)
+    
     for i, env in enumerate(ReplayBuffer(N_EPISODES)):
-        loss = run_episode(model, env, optimizer, criterion)
+        loss = run_episode(model, env, optimizer, criterion) if not random \
+                else run_random_episode(model, env, optimizer, criterion)
 
         lengths.append(len(env.path))
         losses.append(loss.detach().numpy())
 
-        loss_ax.clear()
-        length_ax.clear()
-
-        loss_ax.set_title("Loss Plot")
-        length_ax.set_title("Length Plot")
-
-        loss_ax.plot(losses)
-        length_ax.plot(lengths)
-
         if i % PRINT_FREQUENCY == PRINT_FREQUENCY - 1:
-            graph_ax.clear()
-            graph_ax.set_title(f"{env.path}")
-            print(f"Epoch {i + 1}: loss = {loss.item()}\t path len = {len(env.path)}\t path = {env.path}")
-            nx_graph = pyg.utils.to_networkx(env.initial_graph, to_undirected=True)  # type: ignore
-            color_offset = 50
-            colors = [color_offset + env.path.index(v) if v in env.path else 0 for v in list(nx_graph.nodes)]
-            nx.draw_kamada_kawai(nx_graph, with_labels=True, node_color=colors,  # type: ignore
-                                 ax=graph_ax, cmap=plt.cm.Blues)  # type: ignore
+            # Print epoch info
+            print(f"Epoch {i + 1}: path len = {len(env.path)}\t path = {env.path}")
 
-        plt.pause(0.01)
+            if visualize:
+                graph_ax.clear()
+                graph_ax.set_title("Graph")
 
+                # Labels
+                path_strs = list(map(str, env.path))
+                line_length = 10
+                lines = [", ".join(path_strs[i:i+line_length]) for i in range(0, len(env.path), line_length)]
+                path_multiline = ",\n".join(lines)
+                graph_ax.set_xlabel(f"[{path_multiline}]\nLength: {len(env.path)}", multialignment='left')
+
+                # Drawing
+                nx_graph = pyg.utils.to_networkx(env.initial_graph, to_undirected=True)  # type: ignore
+                pos = nx.kamada_kawai_layout(nx_graph) # type: ignore
+                non_path_nodes = [v for v in nx_graph.nodes if v not in env.path]
+                nx.draw_networkx_nodes(nx_graph, pos, nodelist=non_path_nodes, ax=graph_ax, node_color="gray")  # type: ignore
+                colors = list(range(len(env.path)))  # match the gradient to the ordering of `env.path`
+                nx.draw_networkx_nodes(nx_graph, pos, nodelist=env.path, ax=graph_ax, node_color=colors, cmap=plt.cm.Blues)  # type: ignore
+                nx.draw_networkx_edges(nx_graph, pos, ax=graph_ax) # type: ignore
+                nx.draw_networkx_labels(nx_graph, pos, ax=graph_ax) # type: ignore
+
+        if visualize:
+            loss_ax.clear()
+            length_ax.clear()
+
+            loss_ax.set_title("Loss Plot")
+            length_ax.set_title("Length Plot")
+
+            loss_ax.plot(losses)
+            length_ax.plot(lengths)
+            if notebook:
+                display(fig)
+                clear_output(wait=True)
+            plt.pause(0.01)
+        
+    print("Mean path length:", np.mean(lengths), "\tMean length among last 250:", np.mean(lengths[:-250]))
+
+def main():
+    train_model()
 
 if __name__ == "__main__":
     main()
